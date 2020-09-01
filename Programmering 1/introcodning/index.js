357a95cb384af3e957dabf3ef3ff8969815fe5a4
davidkabyael19A <david.kabayel@elev.ga.ntig.se>
2020-09-01 09:40:15 +0200

    jag e bäst

--- Programmering 1/introcodning/index.js
+++ Programmering 1/introcodning/index.js
@@ -1,5 +1,17 @@
 
 // skapar en variabel som heter name och tilldelar det värdet "david"
-let name = "david"
+let name = "david" // sträng
+let age = 29 // heltal, int
 
-console.log(`Hej ${name}`)
+console.log(`Hej ${name} du är ${age} år gammal`)
+
+// if-sats- att göra val
+if (age >=18){
+    console.log("Du är vuxen")}
+    
+ else {
+    console.log("Du är barn")
+}
+// DOM - Document Object Model  
+let text = document.querySelector("#text")
+text.innerText = "hej"
