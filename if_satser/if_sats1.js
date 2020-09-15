80a65d3b4e12aa8c2666afced867d7ea38b69619
davidkabyael19A <david.kabayel@elev.ga.ntig.se>
2020-09-15 14:01:53 +0200

    jA

---
+++ if_satser/if_sats1.js
@@ -0,0 +1,17 @@
+let tal = prompt ("vilket tal")
+console.log (`tal: ${tal}`)
+//avgöra om  talet är jämnt eller udda
+console.log(`${20%2}`)
+
+// tal%2 är 0: positivt
+
+// tal%2 är 1: negativt
+
+
+if (tal%2 === 1){
+    console.log("positivt")
+}else {console.log("negativt")
+}
+
+//1. låt användaren in ett tale
+//2.avgör om talet är positivt eller negativt
