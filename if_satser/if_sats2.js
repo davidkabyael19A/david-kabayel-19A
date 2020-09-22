089e21d8531d33ea46656c37fc8ef0c47aa34da2
davidkabyael19A <70261035+davidkabyael19A@users.noreply.github.com>
2020-09-22 08:57:44 +0200

    hre

---
+++ if_satser/if_sats2.js
@@ -0,0 +1,6 @@
+let weight = prompt ("hur mycket vikt har du med dig?")
+if (weight >=8) {
+    console.log("du får inte ta med dig väskan")
+} 
+else if (weight <=8) 
+console.log("du får ta med dig")
