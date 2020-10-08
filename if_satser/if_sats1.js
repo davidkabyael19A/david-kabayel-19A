867ffcdc8f251c27af90f37d6de79788c4da97a6
davidkabyael19A <70261035+davidkabyael19A@users.noreply.github.com>
2020-10-08 09:15:22 +0200

    hej

--- if_satser/if_sats1.js
+++ if_satser/if_sats1.js
@@ -1,17 +1,36 @@
-let tal = prompt ("vilket tal")
-console.log (`tal: ${tal}`)
-//avgöra om  talet är jämnt eller udda
-console.log(`${20%2}`)
+let p_talserie1 = document.querySelector("#talserie1")
+let p_talserie2 = document.querySelector("#talserie2")
+let p_summa = document.querySelector("#summa")
+let p_klass = document.querySelector("#klasslista")
+let p_Udda = document.querySelector("#Udda")
+let p_Jämna = document.querySelector("#Jämna")
+let summa = 0
 
-// tal%2 är 0: positivt
-
-// tal%2 är 1: negativt
-
-
-if (tal%2 === 1){
-    console.log("positivt")
-}else {console.log("negativt")
+Jämna.innerHTML = "Jämna tal från 0 till 100: ";
+for(let i=1; i<=100; i++){
+    if( i % 2==0){
+    Jämna.innerHTML += `${i},`;
+    }
+}
+Udda.innerHTML = "Udda tal från 0 till 100: "
+for(let j=1; j<=100; j++){
+    if(j % 2==1){
+        Udda.innerHTML += `${j},`;
+    }
 }
 
-//1. låt användaren in ett tale
-//2.avgör om talet är positivt eller negativt
+
+for (let i = 0; i<10; i++){
+console.log(`${i}. I will not write all over the walls+`)
+}
+
+
+let ämnen = ["Svenska", "Engelska" , "Programmering" , "Matematik" , "Idrott"]
+
+// skapa en lista med lektioner
+
+p_klass.innerHTML = "Mina lektioner är <br/>"
+
+for (let k =0; k < ämnen.length; k++){
+    p_klass.innerHTML += `${k+1} ${ämnen[k]} <br/>`
+}
