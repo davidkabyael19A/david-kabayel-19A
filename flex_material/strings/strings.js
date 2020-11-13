d4908619a26f665ff8b401be3f0006df2e85b6b1
davidkabyael19A <70261035+davidkabyael19A@users.noreply.github.com>
2020-11-13 10:00:51 +0100

    hej

---
+++ flex_material/strings/strings.js
@@ -0,0 +1,23 @@
+console.log("strings")
+
+let fornamn = "david"
+let efternamn = "kabayel"
+let namn = fornamn + "  " + efternamn
+let adress = "kronhusgatan 9"
+let telfon = 112
+let alder = 17
+console.log(namn)
+
+console.log(namn +"\n" + "adress: " + adress)
+let p_uppgifter = dokument.queryselector("#personuppgifter")
+p_uppgifter.innerhtml = "namn: " + namn + "<br>" + "adress: " + adress + "<br>"+"ålder: " + alder
+let alfabet = "abcdefghijklmnopqrstuwyzåäö"
+let bokstav1 = alfabet[0]
+let bokstav1 = alfabet[4]
+console.log(`bokstav på index är 0 är: ${bokstav1}`)
+console.log(`bokstav på index är 4 är: ${bokstav5}`)
+
+let antal_bokstaver = alfabet.length
+console.log(`antal bokstäver i alfabetet är ${antal_bokstaver}`)
+
+
